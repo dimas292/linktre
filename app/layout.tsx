81261ef4bec12a017bd9b3dc6543f4da2e37f749
dimas <dimasixc22@gmail.com>
2024-09-15 11:39:53 +0700

    feat : add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dimas's Portfolio",
+  title: "linktre",
   description: "Modern Portfolio",
 };
 
